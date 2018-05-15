3fb4fed4cab04a72e47bd93bfaaf0231c5e8cf37
caiodiassl <caiodias.sl@gmail.com>
2018-05-15 10:42:39 -0300

    Teste alerta

--- js/functions.js
+++ js/functions.js
@@ -13,6 +13,7 @@ var robo=[
 
 //pega primeira pergunta e exibe na tela
 function pushQuestion(){
+            alert("caio");
             $("#answer_robo").empty();
             firstElement = robo.shift();   
             if (firstElement!=undefined){
