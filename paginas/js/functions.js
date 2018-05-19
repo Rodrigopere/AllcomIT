63294978bd8dbef80303877aba4642c65ec13028
santiagoweb <39306291+santiagoweb@users.noreply.github.com>
2018-05-19 20:55:14 -0300

    commit 21h
    teste

--- paginas/js/functions.js
+++ paginas/js/functions.js
@@ -207,3 +207,30 @@ function typingEffect (str, el, callback) {
       el.innerHTML += next;
     }, 30);
   }
+
+
+
+
+  /*
+===============================================================
+
+Hi! Welcome to my little playground!
+
+My name is Tobias Bogliolo. 'Open source' by default and always 'responsive',
+I'm a publicist, visual designer and frontend developer based in Barcelona. 
+
+Here you will find some of my personal experiments. Sometimes usefull,
+sometimes simply for fun. You are free to use them for whatever you want 
+but I would appreciate an attribution from my work. I hope you enjoy it.
+
+===============================================================
+*/
+
+function openModal (modal){
+    document.getElementById(modal).style.display="block";
+}
+
+
+function closeModal(modal){
+    document.getElementById(modal).style.display="none";
+};
