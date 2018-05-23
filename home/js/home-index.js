4a929db454d30d6265c1a6379ea04a42955d418c
BraiamSerafim <39460733+BraiamSerafim@users.noreply.github.com>
2018-05-23 08:11:05 -0300

    Correção de itens
    Centralizar botão "Iniciar Negociação";
    Pulo de foco das entradas (não consegui colocar foco no botão Enviar);

--- home/js/home-index.js
+++ home/js/home-index.js
@@ -13,6 +13,14 @@ var fields = [
     }
 ];
 
+// $('input.date-style').on('keyup', function() {
+//     console.log('keyup');
+//     console.log($(this).val());
+//     if ($(this).val()) {
+//         $(this).next().focus();
+//     }
+// });
+
 function isValid() {
     return fields.every((field) => {
         return field.Value != '';
@@ -23,7 +31,7 @@ function tryValidate() {
     if (isValid()) {
         var validImg = document.getElementById('validImg').style.display = 'block';
         var elements = document.getElementsByClassName('date-style');
-        for (var i=0; i < elements.length; i++) {
+        for (var i = 0; i < elements.length; i++) {
             var element = elements.item(i);
             element.classList.add('codigo-input-valido');
         }
@@ -42,7 +50,7 @@ function tryValidate() {
     else {
         document.getElementById('validImg').style.display = 'none';
         var elements = document.getElementsByClassName('date-style');
-        for (var i=0; i < elements.length; i++) {
+        for (var i = 0; i < elements.length; i++) {
             var element = elements.item(i);
             element.classList.remove('codigo-input-valido');
         }
@@ -64,7 +72,24 @@ function textChanged(elementName, index) {
     var fieldCode = document.getElementById(elementName).value;
     // console.log('Field ' + index + ' code: ' + fieldCode);
     fields[index].Value = fieldCode;
+    // setFocusToNextInput(index);
     tryValidate();
+}
+
+function setFocusToNextInput(currentIndex) {
+    
+    if (currentIndex < fields.length - 1) {
+        var inputs = document.getElementsByClassName('date-style');
+        // for (var i = 0; i < inputs.length; i++) {
+    console.log(currentIndex);        
+        var nextInput = inputs.item(currentIndex + 1);
+        nextInput.focus();
+        // }
+    }
+    else {
+        var button = document.getElementById('confirmCodeButton');
+        button.focus();
+    }
 }
 
 function getInputCode() {
@@ -81,3 +106,23 @@ function confirmCode() {
     /*Executar validação do código aqui*/
     console.log('Confirmando código: ' + code);
 }
+
+$(document).ready(function(){
+    // console.log('document ready');
+    $('.date-style').keyup(function(){
+        // console.log('keyup');
+        var currentInput = $(this);
+        if(currentInput.val().length==currentInput.attr("maxlength")){
+            // console.log('focus');
+            var nextInput = currentInput.parent().next().children('.date-style')[0];
+            if ($(currentInput).attr('id') == 'inputField_4') {
+                var confirmCodeButton = $('#confirmCodeButton');
+                // console.log(confirmCodeButton);
+                confirmCodeButton.focus();
+            }
+            else {
+                nextInput.focus();
+            }
+        }
+    });
+});
